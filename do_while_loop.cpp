d5495143f65cd869d26725399450b2413fd50e63
Shivam Rane <shivamprane@gmail.com>
2021-12-23 20:50:09 +0530

    Create do_while_loop.cpp

---
+++ do_while_loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i =1, num1, result;
+
+cout << "Enter a number" << endl;
+cin >> num1;
+cout << "multiplication table:" << endl;
+    do
+    {
+        
+     
+        result = num1 * i;       
+        cout << num1 << " x " << i << " = " << result << endl;
+       
+          i++;
+    
+    }while(i<=10);
+
+    return 0;
+
+}
